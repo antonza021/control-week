276c528c30155ff9e4775c4e01a68d2e06b25f26
User <User@WIN-AVGURKPU8OU>
2023-10-16 21:18:43 +0700

    Добавьте файлы проекта.

---
+++ SumSeriesLib/Calculation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumSeriesLib
+{
+    public class Calculation
+    {
+        public static List<double> CalculateSumSeries(double epsilon, double x)
+        {
+            List<double> results = new List<double>();
+            double term = x;
+            double sum = term;
+            int n = 1;
+
+            results.Add(term);
+            results.Add(sum);
+
+            while (Math.Abs(term) >= epsilon)
+            {
+                term *= -x * x * (2 * n - 1) / (2 * n);
+                sum += term;
+                n++;
+                results.Add(term);
+                results.Add(sum);
+            }
+
+            return results;
+        }
+    }
+}
---
+++ controll/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using SumSeriesLib;
+
+namespace controll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double epsilon = 0.0001;
+            double x = 0.5;
+
+            var results = Calculation.CalculateSumSeries(epsilon, x);
+
+            for (int i = 0; i < results.Count; i += 2)
+            {
+                double term = results[i];
+                double sum = results[i + 1];
+                Console.WriteLine($"Term: {term}, Sum: {sum}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
